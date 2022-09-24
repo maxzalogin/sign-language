91e0db63b439ac944ffb42b5e02df16c7409533e
maxzalogin <max.zalogin@gmail.com>
2022-09-24 14:59:51 +0300

    Upload files

---
+++ js/mode.js
@@ -0,0 +1,10 @@
+// Выбираем кнопку
+
+const btn = document.querySelector('.mode-icon');
+btn.addEventListener("click", function () {
+	document.body.classList.toggle("dark-theme");
+});
+// btn.addEventListener("click", function () {
+// document.body.classList.toggle("dark-theme");
+// });
+
